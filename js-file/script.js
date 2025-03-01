8e57c116eae57a4a6c11efce0a27a48bb1c75c4b
Your Name <you@example.com>
2025-03-02 05:03:50 +0600

    replace blog.html with index.html; update navigation links and button functionality

--- js-file/script.js
+++ js-file/script.js
@@ -50,7 +50,7 @@ clearHistory.addEventListener("click", function (event) {
 })
 const headerCard = document.getElementById("header-card");
 headerCard.addEventListener("click", function (event) {
-    window.location.href = "./HTML-FILE/blog.html";
+    window.location.href = "../HTML-FILE/index.html";
 });
 function todayDate() {
     const date = new Date();
